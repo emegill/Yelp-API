4add7c32b1728bfef925e8f438bc921c0aa6cd43
Ethan Megill <megill1352@gmail.comm>
2017-05-03 12:10:38 -0400

    first commit, needs styling

---
+++ scripts.js
@@ -0,0 +1,64 @@
+var searchTerm = document.getElementsByClassName("searchTerm")[0];
+var searchLocation = document.getElementsByClassName("searchLocation")[0];
+var runFunc = document.getElementsByClassName("btn6")[0];
+var results = document.getElementsByClassName("results")[0];
+
+runFunc.addEventListener("click", function(){
+results.innerHTML = " ";
+    
+
+$.ajax({
+ url: "https://yelp-search.herokuapp.com/search",
+ method: "GET",
+ data: {
+ 	term: searchTerm.value,
+	location: searchLocation.value
+},
+ success: function(response){
+     console.log(response)
+    response.businesses.forEach(function(business){
+    yelpApi(business);
+})
+
+//  for(let i = 0; i < response.businesses.length ; i++){
+
+// 	console.log(response.businesses[i]);
+// 	console.log(response.businesses[i].location);
+
+// 	yelpApi(response.businesses[i]);
+
+// }
+}
+})
+
+function yelpApi(business) {
+    var searchResults = document.getElementsByClassName("results")[0];
+    var resultDiv = "<div class='output'><img src='";
+    resultDiv += business.image_url;
+    resultDiv += "'> ";
+            resultDiv += "<div class='name'>";
+            resultDiv += business.name;
+                resultDiv += "<div class='location'>";
+                resultDiv += business.location.address + " " +"Philadelphia, PA";
+                resultDiv += "</div>";
+    			resultDiv += "</div></div>";
+                
+    
+    searchResults.innerHTML += resultDiv;
+
+   
+}
+})
+
+
+// Array.prototype.forEach.call(response.businesses, function(innerArrary){
+
+
+
+
+
+
+
+
+
+
